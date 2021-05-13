fc78495f3d686fc2b84fc241a0d080b17e6f0937
dhirajk1108 <66967734+dhirajk1108@users.noreply.github.com>
2021-05-14 01:30:08 +0530

    initial commit
    initial commit

---
+++ school_master_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO `student`.`school_tbl` (`school_id`, `name`) VALUES ('33-9516954', 'Rose International School')
+INSERT INTO `student`.`school_tbl` (`school_id`, `name`) VALUES ('45-0673066', 'SVP International School')
+INSERT INTO `student`.`school_tbl` (`school_id`, `name`) VALUES ('02-6957378', 'Lotus Public School')
+INSERT INTO `student`.`school_tbl` (`school_id`, `name`) VALUES ('04-5597628', 'SS Public School')
